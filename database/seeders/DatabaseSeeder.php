a94fc623975b691664d1e9168205052dd0ba2fef
alex-s-seo-kr-ua <alex.s@seo.kr.ua>
2023-08-15 22:20:16 +0300

    init application

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,7 +3,10 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\User;
 use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Role;
+
 
 class DatabaseSeeder extends Seeder
 {
@@ -14,9 +17,17 @@ class DatabaseSeeder extends Seeder
     {
         // \App\Models\User::factory(10)->create();
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        $admin = User::factory()->create([
+            'name' => 'Administrator',
+            'email' => 'administrator@gmail.com',
+        ]);
+        $user = User::factory()->create([
+            'name' => 'Guest',
+            'email' => 'guest@gmail.com',
+        ]);
+        $roleUser = Role::create(['name' => 'User']);
+        $roleAdmin = Role::create(['name' => 'Administrator']);
+        $admin->assignRole($roleAdmin);
+        $user->assignRole($roleUser);
     }
 }
